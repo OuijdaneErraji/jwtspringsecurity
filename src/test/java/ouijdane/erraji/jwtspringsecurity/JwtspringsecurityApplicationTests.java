4589e8d5a1b3a0f9015279cbf35d092a26cc0f79
ouijd <ouijdane.erraji@edu.uca.ma>
2021-06-21 18:44:07 +0100

    wij1.0

---
+++ src/test/java/ouijdane/erraji/jwtspringsecurity/JwtspringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ouijdane.erraji.jwtspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtspringsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
